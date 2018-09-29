f660d760ba9efdeafdf46a9b05f1182a9ab53af3
Ivan Sadikov <ivan.sadikov@gmail.com>
2018-09-29 21:35:01 +0200

    add lock

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,14 @@ pub mod params;
 pub mod result;
 pub mod search;
 
+use std::env;
+use std::fs;
+use std::panic;
+use std::path;
+use std::process;
+use std::thread;
+use std::time;
+
 use futures::{future, Stream};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::header::CONTENT_TYPE;
@@ -24,8 +32,13 @@ use hyper::service::service_fn;
 
 type BoxFuture = Box<Future<Item=Response<Body>, Error=hyper::Error> + Send>;
 
+/// File name for storing connection parameters.
+const PARAMS: &str = "PARAMS";
+const LOCK: &str = "LOCK";
+const LOCK_WAIT_MILLIS: u64 = 100;
+
 /// Function to search and return JSON result.
-fn find(params: params::Options) -> Result<String, errors::Error> {
+fn find(params: params::QueryParams) -> Result<String, errors::Error> {
   let res = search::find(params.dir(), params.pattern(), Vec::new())?;
   json::to_string(&res).map_err(|err| errors::Error::new(err.to_string()))
 }
@@ -38,7 +51,7 @@ fn service(req: Request<Body>) -> BoxFuture {
         .concat2()
         .map(move |chunk| {
           let body = chunk.iter().cloned().collect::<Vec<u8>>();
-          match json::from_slice::<params::Options>(&body) {
+          match json::from_slice::<params::QueryParams>(&body) {
             Ok(params) => {
               match find(params) {
                 Ok(payload) => {
@@ -74,11 +87,74 @@ fn service(req: Request<Body>) -> BoxFuture {
   }
 }
 
+/// Creates and synchronises lock.
+fn sync_lock(lock: &path::Path) {
+  let interval = time::Duration::from_millis(LOCK_WAIT_MILLIS);
+  let max_interval = time::Duration::from_millis(100 * LOCK_WAIT_MILLIS);
+  let now = time::Instant::now();
+  while let Err(cause) = fs::OpenOptions::new().write(true).create_new(true).open(lock) {
+    thread::sleep(interval);
+    assert!(now.elapsed() <= max_interval, "Failed to acquire the lock: {}", cause);
+  }
+}
+
+/// Drops existing lock.
+fn drop_lock(lock: &path::Path) {
+  if let Err(cause) = fs::remove_file(lock) {
+    eprintln!("Failed to drop the lock: {}", cause);
+  }
+}
+
+fn with_lock<F, T>(dir: &path::Path, func: F) -> T
+    where F: Fn() -> T + panic::UnwindSafe {
+  let lock = dir.join(LOCK);
+  sync_lock(lock.as_path());
+  let res = panic::catch_unwind(func);
+  drop_lock(lock.as_path());
+  match res {
+    Ok(answer) => answer,
+    Err(cause) => panic!("Internal error: {:?}", cause)
+  }
+}
+
+/// Load connection parameters, if available.
+fn load_connection_params() -> Option<params::ConnectionParams> {
+  let dir = env::current_dir().expect("Failed to retrieve current dir");
+  let dir = dir.as_path();
+  let res: Result<params::ConnectionParams, errors::Error> = with_lock(dir, || {
+    let bytes = fs::read(dir.join(PARAMS))?;
+    let opts = json::from_slice(&bytes)
+      .map_err(|err| errors::Error::new(err.to_string()))?;
+    Ok(opts)
+  });
+  res.ok()
+}
+
+/// Save connection parameters for this process.
+fn save_connection_params(opts: &params::ConnectionParams) -> Option<()> {
+  let dir = env::current_dir().expect("Failed to retrieve current dir");
+  let dir = dir.as_path();
+  let res: Result<(), errors::Error> = with_lock(dir, || {
+    let bytes = json::to_vec(opts).map_err(|err| errors::Error::new(err.to_string()))?;
+    fs::write(dir.join(PARAMS), &bytes)?;
+    Ok(())
+  });
+  res.ok()
+}
+
 fn main() {
-  // Socket address, bind to any available port
-  let addr = ([127, 0, 0, 1], 0).into();
-  let server = Server::bind(&addr)
-    .serve(|| service_fn(service));
-  println!("{}", server.local_addr());
-  hyper::rt::run(server.map_err(|e| eprintln!("Server error: {}", e)));
+  match load_connection_params() {
+    Some(params) => {
+      println!("{}", params.address());
+    },
+    None => {
+      let initial_addr = ([127, 0, 0, 1], 0).into();
+      let server = Server::bind(&initial_addr)
+        .serve(|| service_fn(service));
+      let opts = params::ConnectionParams::new(server.local_addr(), process::id());
+      save_connection_params(&opts);
+      println!("{}", opts.address());
+      hyper::rt::run(server.map_err(|e| eprintln!("Server error: {}", e)));
+    }
+  }
 }
--- src/params.rs
+++ src/params.rs
@@ -1,16 +1,17 @@
+use std::net::SocketAddr;
 use std::path::Path;
 
 use ext::Extension;
 
 /// Input struct that deserialized from JSON payload.
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Options {
+pub struct QueryParams {
   dir: String,
   pattern: String,
   extensions: Option<Vec<String>>
 }
 
-impl Options {
+impl QueryParams {
   /// Returns root directory.
   pub fn dir(&self) -> &Path {
     &Path::new(&self.dir)
@@ -27,5 +28,29 @@ impl Options {
       Some(vec) => vec.iter().map(|ext| ext.parse().unwrap()).collect(),
       None => Vec::new()
     }
+  }
+}
+
+/// Connection params that are returned when server is started.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct ConnectionParams {
+  address: SocketAddr,
+  pid: u32
+}
+
+impl ConnectionParams {
+  /// Creates new connection params.
+  pub fn new(address: SocketAddr, pid: u32) -> Self {
+    Self { address, pid }
+  }
+
+  /// Returns address to connect.
+  pub fn address(&self) -> SocketAddr {
+    self.address
+  }
+
+  /// Returns process id.
+  pub fn pid(&self) -> u32 {
+    self.pid
   }
 }
